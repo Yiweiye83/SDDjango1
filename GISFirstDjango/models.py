ac5fbceb8e8fbe667923d88f21b4a4d019c44030
204565 <204565@gardenschool.edu.my>
2024-01-30 10:32:49 +0800

    add two person

--- GISFirstDjango/models.py
+++ GISFirstDjango/models.py
@@ -5,5 +5,7 @@ class Person(models.Model):
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
 
+    def __str__(self):
+        return self.first_name + " " + self.last_name
 class TeachingGroup(models.Model):
     students = models.OneToOneField(Person, null=True, on_delete=models.SET_NULL)
